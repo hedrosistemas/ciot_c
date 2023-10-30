a0265992a6671652c1bd9e62d551aa8cb8c13823
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-10-30 10:36:15 -0300

    feat: update message len paramenter

--- main/ciot_config.h
+++ main/ciot_config.h
@@ -24,7 +24,7 @@
 #define CIOT_CONFIG_FEATURE_HTTPS 1    ///!< Enable HTTP Server
 #define CIOT_CONFIG_FEATURE_HTTPC 1    ///!< Enable HTTP Client
 #define CIOT_CONFIG_FEATURE_MQTTC 1    ///!< Enable MQTT Client
-#define CIOT_CONFIG_MESSAGE_LEN 334    ///!< CIOT expected message size
+#define CIOT_CONFIG_MESSAGE_LEN 330    ///!< CIOT expected message size
 
 #define CIOT_CONFIG_UART_RX_BUF_SIZE 256
 #define CIOT_CONFIG_UART_TX_BUF_SIZE 256
