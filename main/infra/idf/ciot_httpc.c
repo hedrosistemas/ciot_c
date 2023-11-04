0ce45c919e15ee2a3a1893c94d025bb27eb9ebd9
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-11-03 21:00:05 -0300

    chore: rename 'this' to 'self'

--- main/infra/idf/ciot_httpc.c
+++ main/infra/idf/ciot_httpc.c
@@ -33,40 +33,40 @@ static const char *TAG = "ciot_httpc";
 
 ciot_httpc_t ciot_httpc_new(void *handle)
 {
-    ciot_httpc_t this = calloc(1, sizeof(struct ciot_httpc));
-    this->iface.base.ptr = this;
-    this->iface.base.start = (ciot_iface_start_fn *)ciot_httpc_start;
-    this->iface.base.stop = (ciot_iface_stop_fn *)ciot_httpc_stop;
-    this->iface.base.process_req = (ciot_iface_process_req_fn *)ciot_httpc_process_req;
-    this->iface.base.send_data = (ciot_iface_send_data_fn *)ciot_httpc_send_data;
-    this->iface.base.cfg.ptr = &this->cfg;
-    this->iface.base.cfg.size = sizeof(this->cfg);
-    this->iface.base.status.ptr = &this->status;
-    this->iface.base.status.size = sizeof(this->status);
-    this->iface.info.type = CIOT_IFACE_TYPE_HTTP_SERVER;
-    return this;
+    ciot_httpc_t self = calloc(1, sizeof(struct ciot_httpc));
+    self->iface.base.ptr = self;
+    self->iface.base.start = (ciot_iface_start_fn *)ciot_httpc_start;
+    self->iface.base.stop = (ciot_iface_stop_fn *)ciot_httpc_stop;
+    self->iface.base.process_req = (ciot_iface_process_req_fn *)ciot_httpc_process_req;
+    self->iface.base.send_data = (ciot_iface_send_data_fn *)ciot_httpc_send_data;
+    self->iface.base.cfg.ptr = &self->cfg;
+    self->iface.base.cfg.size = sizeof(self->cfg);
+    self->iface.base.status.ptr = &self->status;
+    self->iface.base.status.size = sizeof(self->status);
+    self->iface.info.type = CIOT_IFACE_TYPE_HTTP_SERVER;
+    return self;
 }
 
-ciot_err_t ciot_httpc_start(ciot_httpc_t this, ciot_httpc_cfg_t *cfg)
+ciot_err_t ciot_httpc_start(ciot_httpc_t self, ciot_httpc_cfg_t *cfg)
 {
-    if (this->status.state == CIOT_HTTPC_STATE_IDLE)
+    if (self->status.state == CIOT_HTTPC_STATE_IDLE)
     {
-        CIOT_NULL_CHECK(this);
+        CIOT_NULL_CHECK(self);
         CIOT_NULL_CHECK(cfg);
-        memcpy(&this->cfg, cfg, sizeof(this->cfg));
+        memcpy(&self->cfg, cfg, sizeof(self->cfg));
         esp_http_client_config_t req_cfg = {0};
-        req_cfg.url = this->cfg.url;
-        req_cfg.transport_type = this->cfg.transport;
-        req_cfg.method = this->cfg.method;
-        req_cfg.user_data = this;
+        req_cfg.url = self->cfg.url;
+        req_cfg.transport_type = self->cfg.transport;
+        req_cfg.method = self->cfg.method;
+        req_cfg.user_data = self;
         req_cfg.event_handler = ciot_httpc_event_handler;
-        this->status.state = CIOT_HTTPC_STATE_CONNECTING;
+        self->status.state = CIOT_HTTPC_STATE_CONNECTING;
 #ifdef CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
-        if (this->cfg.transport == CIOT_HTTPC_TRANSPORT_SSL)
+        if (self->cfg.transport == CIOT_HTTPC_TRANSPORT_SSL)
         {
             req_cfg.crt_bundle_attach = esp_crt_bundle_attach;
         }
-        this->handle = esp_http_client_init(&req_cfg);
+        self->handle = esp_http_client_init(&req_cfg);
 #endif
         return CIOT_OK;
     }
@@ -76,27 +76,27 @@ ciot_err_t ciot_httpc_start(ciot_httpc_t this, ciot_httpc_cfg_t *cfg)
     }
 }
 
-ciot_err_t ciot_httpc_stop(ciot_httpc_t this)
+ciot_err_t ciot_httpc_stop(ciot_httpc_t self)
 {
-    return esp_http_client_cleanup(this->handle);
+    return esp_http_client_cleanup(self->handle);
 }
 
-ciot_err_t ciot_httpc_process_req(ciot_httpc_t this, ciot_httpc_req_t *req)
+ciot_err_t ciot_httpc_process_req(ciot_httpc_t self, ciot_httpc_req_t *req)
 {
     return CIOT_ERR_NOT_SUPPORTED;
 }
 
-ciot_err_t ciot_httpc_send_data(ciot_httpc_t this, uint8_t *data, int size)
+ciot_err_t ciot_httpc_send_data(ciot_httpc_t self, uint8_t *data, int size)
 {
-    if (this->status.state == CIOT_HTTPC_STATE_STARTED)
+    if (self->status.state == CIOT_HTTPC_STATE_STARTED)
     {
-        esp_http_client_set_post_field(this->handle, (char*)data, size);
-        esp_http_client_perform(this->handle);
+        esp_http_client_set_post_field(self->handle, (char*)data, size);
+        esp_http_client_perform(self->handle);
     }
     return CIOT_ERR_NOT_IMPLEMENTED;
 }
 
-static void ciot_httpc_event_data(ciot_httpc_t this, ciot_iface_event_t *event, char *data, int size)
+static void ciot_httpc_event_data(ciot_httpc_t self, ciot_iface_event_t *event, char *data, int size)
 {
     event->id = CIOT_IFACE_EVENT_DATA;
     event->size = size;
@@ -105,20 +105,20 @@ static void ciot_httpc_event_data(ciot_httpc_t this, ciot_iface_event_t *event, 
 
 static int ciot_httpc_event_handler(esp_http_client_event_handle_t evt)
 {
-    ciot_httpc_t this = evt->user_data;
+    ciot_httpc_t self = evt->user_data;
     ciot_iface_event_t event = {0};
 
     switch (evt->event_id)
     {
     case HTTP_EVENT_ERROR:
         ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
-        this->status.state = CIOT_HTTPC_STATE_ERROR;
+        self->status.state = CIOT_HTTPC_STATE_ERROR;
         event.id = CIOT_IFACE_EVENT_ERROR;
-        event.msg.data.httpc.status = this->status;
+        event.msg.data.httpc.status = self->status;
         break;
     case HTTP_EVENT_ON_CONNECTED:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
-        this->status.state = CIOT_HTTPC_STATE_CONNECTED;
+        self->status.state = CIOT_HTTPC_STATE_CONNECTED;
         break;
     case HTTP_EVENT_HEADERS_SENT:
         ESP_LOGD(TAG, "HTTP_EVENT_HEADERS_SENT");
@@ -128,15 +128,15 @@ static int ciot_httpc_event_handler(esp_http_client_event_handle_t evt)
         break;
     case HTTP_EVENT_ON_DATA:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA");
-        ciot_httpc_event_data(this, &event, evt->data, evt->data_len);
+        ciot_httpc_event_data(self, &event, evt->data, evt->data_len);
         break;
     case HTTP_EVENT_ON_FINISH:
         ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-        this->status.state = CIOT_HTTPC_STATE_IDLE;
+        self->status.state = CIOT_HTTPC_STATE_IDLE;
         break;
     case HTTP_EVENT_DISCONNECTED:
         ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
-        this->status.state = CIOT_HTTPC_STATE_IDLE;
+        self->status.state = CIOT_HTTPC_STATE_IDLE;
         break;
     case HTTP_EVENT_REDIRECT:
         ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
@@ -146,9 +146,9 @@ static int ciot_httpc_event_handler(esp_http_client_event_handle_t evt)
         break;
     }
 
-    if(this->iface.event_handler != NULL)
+    if(self->iface.event_handler != NULL)
     {
-        this->iface.event_handler(this, &event, this->iface.event_args);
+        self->iface.event_handler(self, &event, self->iface.event_args);
     }
 
     return ESP_OK;
