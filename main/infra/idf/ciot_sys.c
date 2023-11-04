081f124ae0e6953a59211f82978df79a31804aff
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-11-03 21:00:47 -0300

    refact: refact idf ciot_sys

--- main/infra/idf/ciot_sys.c
+++ main/infra/idf/ciot_sys.c
@@ -21,51 +21,24 @@
 #include "ciot_config.h"
 #include "ciot_sys.h"
 
-typedef struct ciot_sys_handle
+typedef struct ciot_sys
 {
+    ciot_iface_t iface;
     ciot_sys_cfg_t cfg;
     ciot_sys_status_t status;
     time_t init_time;
-} ciot_sys_handle_t;
+} ciot_sys_t;
 
-static ciot_sys_handle_t sys;
+static ciot_sys_t sys;
 
 RTC_NOINIT_ATTR int rst_count;
 
+static void ciot_sys_init(ciot_sys_t self);
 static ciot_sys_status_t *ciot_sys_get_status();
-
-void ciot_sys_init(void)
-{
-    char hw_name[] = CIOT_CONFIG_HARDWARE_NAME;
-    uint8_t fw_ver[] = CIOT_CONFIG_FIRMWARE_VER;
-
-    sys.status.info.features.hw.uart = CIOT_CONFIG_FEATURE_UART;
-    sys.status.info.features.hw.ethernet = CIOT_CONFIG_FEATURE_ETHERNET;
-    sys.status.info.features.hw.wifi = CIOT_CONFIG_FEATURE_WIFI;
-    sys.status.info.features.hw.bluetooth = CIOT_CONFIG_FEATURE_BLE;
-    
-    sys.status.info.features.sw.ntp = CIOT_CONFIG_FEATURE_NTP;
-    sys.status.info.features.sw.ntp = CIOT_CONFIG_FEATURE_OTA;
-    sys.status.info.features.sw.http_client = CIOT_CONFIG_FEATURE_HTTPC;
-    sys.status.info.features.sw.http_server = CIOT_CONFIG_FEATURE_HTTPS;
-    sys.status.info.features.sw.mqtt_client = CIOT_CONFIG_FEATURE_MQTTC;
-
-    if (esp_reset_reason() == ESP_RST_POWERON || esp_reset_reason() == ESP_RST_SW)
-    {
-        rst_count = 0;
-    }
-
-    rst_count++;
-    sys.status.rst_reason = esp_reset_reason();
-    sys.status.rst_count = rst_count;
-
-    memcpy(sys.status.info.hw_name, hw_name, sizeof(hw_name));
-    memcpy(sys.status.info.fw_ver, fw_ver, sizeof(fw_ver));
-}
 
 ciot_sys_t ciot_sys_new(void *handle)
 {
-    ciot_sys_t self = calloc(1, sizeof(ciot_iface_t));
+    ciot_sys_t self = calloc(1, sizeof(struct ciot_sys));
     self->base.ptr = self;
     self->base.start = (ciot_iface_start_fn *)ciot_sys_start;
     self->base.stop = (ciot_iface_stop_fn *)ciot_sys_stop;
@@ -76,6 +49,7 @@ ciot_sys_t ciot_sys_new(void *handle)
     self->base.status.ptr = ciot_sys_get_status;
     self->base.status.size = sizeof(ciot_sys_status_t);
     self->info.type = CIOT_IFACE_TYPE_SYSTEM;
+    ciot_sys_init(self);
     return self;
 }
 
@@ -97,6 +71,35 @@ ciot_err_t ciot_sys_process_req(ciot_sys_t self, ciot_sys_req_t *req)
 ciot_err_t ciot_sys_send_data(ciot_sys_t self, uint8_t *data, int size)
 {
     return CIOT_ERR_NOT_SUPPORTED;
+}
+
+static void ciot_sys_init(ciot_sys_t self)
+{
+    char hw_name[] = CIOT_CONFIG_HARDWARE_NAME;
+    uint8_t fw_ver[] = CIOT_CONFIG_FIRMWARE_VER;
+
+    self->status.info.features.hw.uart = CIOT_CONFIG_FEATURE_UART;
+    self->status.info.features.hw.ethernet = CIOT_CONFIG_FEATURE_ETHERNET;
+    self->status.info.features.hw.wifi = CIOT_CONFIG_FEATURE_WIFI;
+    self->status.info.features.hw.bluetooth = CIOT_CONFIG_FEATURE_BLE;
+    
+    self->status.info.features.sw.ntp = CIOT_CONFIG_FEATURE_NTP;
+    self->status.info.features.sw.ntp = CIOT_CONFIG_FEATURE_OTA;
+    self->status.info.features.sw.http_client = CIOT_CONFIG_FEATURE_HTTPC;
+    self->status.info.features.sw.http_server = CIOT_CONFIG_FEATURE_HTTPS;
+    self->status.info.features.sw.mqtt_client = CIOT_CONFIG_FEATURE_MQTTC;
+
+    if (esp_reset_reason() == ESP_RST_POWERON || esp_reset_reason() == ESP_RST_SW)
+    {
+        rst_count = 0;
+    }
+
+    rst_count++;
+    self->status.rst_reason = esp_reset_reason();
+    self->status.rst_count = rst_count;
+
+    memcpy(self->status.info.hw_name, hw_name, sizeof(hw_name));
+    memcpy(self->status.info.fw_ver, fw_ver, sizeof(fw_ver));
 }
 
 static ciot_sys_status_t *ciot_sys_get_status()
