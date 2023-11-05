9e57c9948a82d62fd5ba5ee78a30f00b42e06001
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-11-05 10:23:50 -0300

    refact: refact win32 sample

--- examples/win32/main.c
+++ examples/win32/main.c
@@ -17,18 +17,22 @@
 #include "ciot.h"
 #include "ciot_log.h"
 #include "ciot_sys.h"
+#include "ciot_storage.h"
 #include "ciot_uart.h"
 #include "ciot_https.h"
 #include "ciot_httpc.h"
 #include "ciot_httpc.h"
 #include "ciot_mqttc.h"
 
-#include "ciot_custom_config.h"
+#include "ciot_config.h"
+
+#define CIOT_IFACES_COUNT 6
 
 typedef struct app
 {
     ciot_t ciot;
     ciot_sys_t sys;
+    ciot_storage_t storage;
     ciot_uart_t uart;
     ciot_https_t https;
     ciot_httpc_t httpc;
@@ -37,11 +41,49 @@ typedef struct app
 } app_t;
 
 static void app_init(app_t *self);
-static void app_start(app_t *self);
-static ciot_err_t app_uart_start(app_t *self);
-static ciot_err_t app_https_start(app_t *self);
-static ciot_err_t app_httpc_start(app_t *self);
-static ciot_err_t app_mqttc_start(app_t *self);
+
+static const ciot_storage_cfg_t storage_cfg = {
+    .type = CIOT_STORAGE_TYPE_FS
+};
+
+static const ciot_uart_cfg_t uart_cfg = {
+    .baud_rate = CIOT_CONFIG_UART_BAUD,
+    .num = CIOT_CONFIG_UART_PORT,
+};
+
+static const ciot_https_cfg_t https_cfg = {
+    .address = CIOT_CONFIG_HTTPS_ADDRESS,
+    .route = CIOT_CONFIG_HTTPS_ROUTE,
+};
+
+static const ciot_httpc_cfg_t httpc_cfg = {
+    .url = CIOT_CONFIG_HTTPC_URL,
+    .transport = CIOT_CONFIG_HTTPC_TRANSPORT,  
+    .method = CIOT_CONFIG_HTTPC_METHOD,
+    .timeout = CIOT_CONFIG_HTTPC_TIMEOUT,
+};
+
+static const ciot_mqttc_cfg_t mqttc_cfg = {
+    .client_id = CIOT_CONFIG_MQTTC_ID,
+    .url = CIOT_CONFIG_MQTTC_URL,
+    .port = CIOT_CONFIG_MQTTC_PORT,
+    .qos = CIOT_CONFIG_MQTTC_QOS,
+    .topics = {
+        .b2d = CIOT_CONFIG_MQTTC_TOPIC_B2D,
+        .d2b = CIOT_CONFIG_MQTTC_TOPIC_D2B,
+    }
+};
+
+static const void *cfgs[] = {
+    &storage_cfg,
+    NULL,
+    &uart_cfg,
+    &https_cfg,
+    &httpc_cfg,
+    &mqttc_cfg,
+};
+
+static ciot_iface_t* ifaces[CIOT_IFACES_COUNT];
 
 static const char *TAG = "main";
 
@@ -49,20 +91,25 @@ int main()
 {
     app_t app;
     app_init(&app);
-    app_start(&app);
 
-    ciot_iface_t* iface_list[] = {
-        (ciot_iface_t*)app.sys,
-        (ciot_iface_t*)app.uart,
-        (ciot_iface_t*)app.https,
-        (ciot_iface_t*)app.httpc,
-        (ciot_iface_t*)app.mqttc,
+    ifaces[0] = (ciot_iface_t*)app.storage;
+    ifaces[1] = (ciot_iface_t*)app.sys;
+    ifaces[2] = (ciot_iface_t*)app.uart;
+    ifaces[3] = (ciot_iface_t*)app.https;
+    ifaces[4] = (ciot_iface_t*)app.httpc;
+    ifaces[5] = (ciot_iface_t*)app.mqttc;
+
+    ciot_cfg_t ciot_cfg = {
+        .ifaces = ifaces,
+        .cfgs = cfgs,
+        .count = CIOT_IFACES_COUNT
     };
-    ciot_set_iface_list(app.ciot, iface_list, sizeof(iface_list) / sizeof(ciot_iface_t*));
+    ciot_start(app.ciot, &ciot_cfg);
 
     CIOT_LOGI(TAG, "App is running...", "");
     while (true)
     {
+        ciot_sys_task(app.sys);
         ciot_uart_task(app.uart);
         mg_mgr_poll(&app.mgr, 1000);
     }
@@ -78,62 +125,9 @@ static void app_init(app_t *self)
     mg_mgr_init(&self->mgr);
     self->ciot = ciot_new();
     self->sys = ciot_sys_new(NULL);
+    self->storage = ciot_storage_new(NULL);
     self->uart = ciot_uart_new(NULL);
     self->https = ciot_https_new(&self->mgr);
     self->httpc = ciot_httpc_new(&self->mgr);
     self->mqttc = ciot_mqttc_new(&self->mgr);
-}
-
-static void app_start(app_t *self)
-{
-    CIOT_LOGI(TAG, "App is starting...", "");
-    app_uart_start(self);
-    app_https_start(self);
-    app_httpc_start(self);
-    app_mqttc_start(self);
-}
-
-static ciot_err_t app_uart_start(app_t *self)
-{
-    ciot_uart_cfg_t uart_cfg = {
-        .baud_rate = CIOT_CONFIG_UART_BAUD,
-        .num = CIOT_CONFIG_UART_PORT,
-    };
-    return ciot_uart_start(self->uart, &uart_cfg);
-}
-
-static ciot_err_t app_https_start(app_t *self)
-{
-    ciot_https_cfg_t https_cfg = {
-        .address = "127.0.0.1",
-        .port = 8080,
-        .route = "/v1/ciot",
-    };
-    return ciot_https_start(self->https, &https_cfg);
-}
-
-static ciot_err_t app_httpc_start(app_t *self)
-{
-    ciot_httpc_cfg_t httpc_cfg = {
-        .url = "v1/ciot",
-        .transport = CIOT_HTTPC_TRANSPORT_TCP,  
-        .method = CIOT_HTTPC_METHOD_POST,
-        .timeout = CIOT_CONFIG_HTTPC_TIMEOUT,
-    };
-    return ciot_httpc_start(self->httpc, &httpc_cfg);
-}
-
-static ciot_err_t app_mqttc_start(app_t *self)
-{
-    ciot_mqttc_cfg_t mqtt_cfg = {
-        .client_id = "mqtt_client_id",
-        .url = "mqtt://test.mosquitto.org",
-        .port = 1883,
-        .qos = 0,
-        .topics = {
-            .b2d = "CIOT/B2D",
-            .d2b = "CIOT/D2B",
-        }
-    };
-    return ciot_mqttc_start(self->mqttc, &mqtt_cfg);
 }
