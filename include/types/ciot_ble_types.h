af0193fc0fe7441b2955561dc7993047d0196e5d
wesley-santos <53940899+WesleyJoseSantos@users.noreply.github.com>
2023-11-28 06:47:06 -0300

    feat: update ciot_ble_types.h

--- include/types/ciot_ble_types.h
+++ include/types/ciot_ble_types.h
@@ -1,16 +1,16 @@
 /**
- * @file ciot_ble_data.h
+ * @file ciot_ble_types.h
  * @author your name (you@domain.com)
  * @brief 
  * @version 0.1
- * @date 2023-10-23
+ * @date 2023-11-20
  * 
  * @copyright Copyright (c) 2023
  * 
  */
 
-#ifndef __CIOT_BLE_DATA__H__
-#define __CIOT_BLE_DATA__H__
+#ifndef __CIOT_BLE_TYPES__H__
+#define __CIOT_BLE_TYPES__H__
 
 #include <inttypes.h>
 
@@ -19,6 +19,7 @@
 typedef enum __attribute__((packed))
 {
     CIOT_BLE_STATE_IDLE,
+    CIOT_BLE_STATE_STARTED,
 } ciot_ble_state_t;
 
 typedef enum __attribute__((packed))
@@ -28,12 +29,21 @@ typedef enum __attribute__((packed))
 
 typedef struct __attribute__((packed))
 {
-
+    bool set_mac;
+    uint8_t mac[6];
 } ciot_ble_cfg_t;
 
 typedef struct __attribute__((packed))
 {
+    uint8_t hw_mac[6];
+    uint8_t sw_mac[6];
+} ciot_ble_info_t;
+
+typedef struct __attribute__((packed))
+{
     ciot_ble_state_t state;
+    ciot_ble_info_t info;
+    int err_code;
 } ciot_ble_status_t;
 
 typedef union __attribute__((packed))
@@ -49,11 +59,11 @@ typedef struct __attribute__((packed))
 
 typedef union __attribute__((packed))
 {
-    #if CIOT_CONFIG_FEATURE_ble
+    // #if CIOT_CONFIG_FEATURE_BLE
     ciot_ble_cfg_t config;
     ciot_ble_status_t status;
     ciot_ble_req_t request;
-    #endif
+    // #endif
 } ciot_ble_data_u;
 
-#endif  //!__CIOT_BLE_DATA__H__
+#endif  //!__CIOT_BLE_TYPES__H__
