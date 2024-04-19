76cc66ef649bd6d8d8306c428b2f3f6bcc569756
wesley-santos <BINARY_QUANTUM@outlook.com>
2024-04-19 16:21:31 -0300

    feat: add other events

--- include/ciot_ota.h
+++ include/ciot_ota.h
@@ -24,7 +24,11 @@ typedef struct ciot_ota *ciot_ota_t;
 
 typedef enum ciot_ota_event_id
 {
-    CIOT_OTA_EVENT_STATE_CHANGED = CIOT_IFACE_EVENT_CUSTOM,
+    CIOT_OTA_EVENT_CONNECTED = CIOT_IFACE_EVENT_CUSTOM,
+    CIOT_OTA_EVENT_READING_INFO,
+    CIOT_OTA_EVENT_DECRYPTING,
+    CIOT_OTA_EVENT_WRITING,
+    CIOT_OTA_EVENT_UPDATING_APP,
 } ciot_ota_event_id_t;
 
 typedef struct __attribute__((packed))
@@ -38,6 +42,7 @@ ciot_err_t ciot_ota_start(ciot_ota_t self, ciot_ota_cfg_t *cfg);
 ciot_err_t ciot_ota_stop(ciot_ota_t self);
 ciot_err_t ciot_ota_process_req(ciot_ota_t self, ciot_ota_req_t *req);
 ciot_err_t ciot_ota_send_data(ciot_ota_t self, uint8_t *data, int size);
+ciot_err_t ciot_ota_rollback(ciot_ota_t self);
 
 #ifdef __cplusplus
 }
