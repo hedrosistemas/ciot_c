abc59bf5715af51ea52f96c70efc12c24b826e9f
wesley-santos <wesleypro37@gmail.com>
2024-05-14 22:05:36 -0300

    feat: moved files

--- docs/html/group__uart__apis.js
+++
@@ -1,9 +0,0 @@
-var group__uart__apis =
-[
-    [ "ciot_crc16_be", "group__uart__apis.html#ga5d59ce0d0ffe65eaf7944d695bde59e3", null ],
-    [ "ciot_crc16_le", "group__uart__apis.html#ga73a6ce791d6f000bad7ee2eedd0d8350", null ],
-    [ "ciot_crc32_be", "group__uart__apis.html#gaeed9b1c0c84b16c8e4e4b65be0787ae4", null ],
-    [ "ciot_crc32_le", "group__uart__apis.html#gad020e1771e2ff7a25a068ccc1b3d1331", null ],
-    [ "ciot_crc8_be", "group__uart__apis.html#gae831c4d3ed93d649df5ed07e5b9d1c46", null ],
-    [ "ciot_crc8_le", "group__uart__apis.html#ga39da2d632baf462933f90f3fbe7a63e7", null ]
-];
