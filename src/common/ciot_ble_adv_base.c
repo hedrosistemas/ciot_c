7e45dde148d259b0668c0bb5b588bc47ce31f5b2
wesley-santos <wesleypro37@gmail.com>
2024-07-24 09:13:28 -0300

    refact: removed unecessary null check

--- src/common/ciot_ble_adv_base.c
+++ src/common/ciot_ble_adv_base.c
@@ -92,8 +92,6 @@ static ciot_err_t ciot_iface_send_data(ciot_iface_t *iface, uint8_t *data, int s
 
 ciot_err_t ciot_ble_adv_process_req(ciot_ble_adv_t self, ciot_ble_adv_req_t *req)
 {
-    CIOT_ERR_NULL_CHECK(self);
-    CIOT_ERR_NULL_CHECK(req);
     return CIOT_ERR__NOT_IMPLEMENTED;
 }
 
