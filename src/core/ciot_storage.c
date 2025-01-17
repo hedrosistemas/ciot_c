827e18e0e6229f656563a640492fa1aaf492470b
wesley-santos <BINARY_QUANTUM@outlook.com>
2025-01-17 06:41:16 -0300

    feat: implement save and load

--- src/core/ciot_storage.c
+++ src/core/ciot_storage.c
@@ -10,13 +10,24 @@
  */
 
 #include "ciot_storage.h"
+#include "ciot_config.h"
+#include "ciot_err.h"
 
 ciot_err_t ciot_storage_save_data(ciot_storage_t self, char *path, ciot_msg_data_t *data)
 {
-    return CIOT_ERR_NOT_IMPLEMENTED;
+    CIOT_ERR_NULL_CHECK(self);
+    uint8_t bytes[CIOT_CONFIG_MSG_SIZE];
+    int size = ciot_serializer_to_bytes(bytes, sizeof(bytes), data, CIOT_MSG_DATA_FIELDS);
+    return self->write_bytes(self, path, bytes, size);
 }
 
 ciot_err_t ciot_storage_load_data(ciot_storage_t self, char *path, ciot_msg_data_t *data)
 {
-    return CIOT_ERR_NOT_IMPLEMENTED;
+    CIOT_ERR_NULL_CHECK(self);
+    int size = 0;
+    CIOT_ERR_RETURN(self->read_bytes(self, path, NULL, &size));
+    uint8_t bytes[CIOT_CONFIG_MSG_SIZE];
+    CIOT_ERR_RETURN(self->read_bytes(self, path, bytes, &size));
+    ciot_serializer_from_bytes(bytes, size, data, CIOT_MSG_DATA_FIELDS);
+    return CIOT_ERR_OK;
 }
