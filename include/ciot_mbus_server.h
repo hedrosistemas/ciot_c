a1d65b6cb96ef646e8f5a36e05c9e99d0eca990c
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-04-17 09:44:17 -0300

    feat: update ciot_mbus_server_new to accept additional parameters for data and interface

--- include/ciot_mbus_server.h
+++ include/ciot_mbus_server.h
@@ -19,6 +19,10 @@ extern "C" {
 #include "ciot_config.h"
 #include "ciot_types.h"
 #include "ciot_iface.h"
+#include "ciot_mbus.h"
+#include "nanomodbus.h"
+
+#define CIOT_MBUS_COIL_WRITE(coils, addr, value) nmbs_bitfield_write(coils, addr, value)
 
 typedef struct ciot_mbus_server *ciot_mbus_server_t;
 
@@ -29,10 +33,11 @@ typedef struct ciot_mbus_server_base
     ciot_mbus_server_status_t status;
 } ciot_mbus_server_base_t;
 
-ciot_mbus_server_t ciot_mbus_server_new(void *handle);
+ciot_mbus_server_t ciot_mbus_server_new(void *handle, ciot_mbus_data_t *data, ciot_iface_t *iface);
 ciot_err_t ciot_mbus_server_init(ciot_mbus_server_t self);
 ciot_err_t ciot_mbus_server_start(ciot_mbus_server_t self, ciot_mbus_server_cfg_t *cfg);
 ciot_err_t ciot_mbus_server_stop(ciot_mbus_server_t self);
+ciot_err_t ciot_mbus_server_task(ciot_mbus_server_t self);
 ciot_err_t ciot_mbus_server_process_req(ciot_mbus_server_t self, ciot_mbus_server_req_t *req);
 ciot_err_t ciot_mbus_server_get_cfg(ciot_mbus_server_t self, ciot_mbus_server_cfg_t *cfg);
 ciot_err_t ciot_mbus_server_get_status(ciot_mbus_server_t self, ciot_mbus_server_status_t *status);
