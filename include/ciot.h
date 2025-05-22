7be50fecedcbc9aae7b4211031398dce996dd1d5
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-05-22 11:31:49 -0300

    feat: add ciot_delete_all function for deleting all configurations

--- include/ciot.h
+++ include/ciot.h
@@ -103,6 +103,7 @@ ciot_err_t ciot_delete_cfg(ciot_t self, ciot_iface_info_t *iface);
 ciot_err_t ciot_save_cfg(ciot_t self, ciot_iface_info_t *iface);
 ciot_err_t ciot_load_cfg(ciot_t self, ciot_iface_info_t *iface, ciot_msg_data_t *cfg);
 ciot_err_t ciot_get_ifaces_info(ciot_t self, ciot_info_t *info);
+ciot_err_t ciot_delete_all(ciot_t self);
 bool ciot_cfg_exists(ciot_t self, uint8_t iface_id);
 const char *ciot_event_to_str(ciot_event_t *event);
 
