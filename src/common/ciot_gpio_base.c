35d8af5090b006303ce5730d369b403294bc79cf
wesley-santos <53940899+wjsan@users.noreply.github.com>
2025-05-22 11:33:51 -0300

    fix: update ciot_gpio_set_cfg to set status states size based on pins_count

--- src/common/ciot_gpio_base.c
+++ src/common/ciot_gpio_base.c
@@ -161,6 +161,7 @@ ciot_err_t ciot_gpio_set_cfg(ciot_gpio_t self, ciot_gpio_cfg_t *cfg)
     CIOT_ERR_INDEX_CHECK(cfg->pins_count, 0, sizeof(cfg->pins) / sizeof(cfg->pins[0]));
     ciot_gpio_base_t *base = (ciot_gpio_base_t*)self;
     base->cfg = *cfg;
+    base->status.states.size = cfg->pins_count;
     return CIOT_ERR_OK;
 }
 
